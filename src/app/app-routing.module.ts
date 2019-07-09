92fca1eaec11b2a596073170af3126f165ec579a
ahsanzaman2489 <ahsan_zami@hotmail.com>
2019-07-09 08:46:53 -0500

    * Bootstrap and jquery added. * init category component * init jokes component * init HTTP service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {CategoriesComponent} from './categories/categories.component';
+import {JokesComponent} from './jokes/jokes.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CategoriesComponent},
+  {path: 'jokes/:category', component: JokesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CategoriesComponent} from './categories/categories.component';
+import {HttpClientModule} from '@angular/common/http';
+import { JokesComponent } from './jokes/jokes.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent,
+    JokesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
